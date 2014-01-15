4a9a4402308de8e38b9ac452e8f8132f354ddc99
GustavoMA <gugamalves@yahoo.com.br>
2014-01-15 14:56:12 -0200

    Update RSA.java

--- edu/ufcg/RSA.java
+++ edu/ufcg/RSA.java
@@ -2,6 +2,35 @@ package edu.ufcg;
 
 public class RSA {
   
+	/*
+	 * Metodo auxiliar para calcular o Maximo Divisor Comum entre A e B.
+	 * ~Gustavo~
+	 */
+	private static int MDC(int a, int b) {
+	    
+		a = Math.abs(a);
+		b = Math.abs(b);
+		    
+		if (a < b) {
+		      
+			int temp;
+		      
+			temp = a;
+			a = b;
+			b = temp;
+		}
+		    
+		while (b != 0) {
+		      
+			int temp;
+		      
+			temp = a;
+			a = b;
+			b = temp % b;
+		}
+
+		return a;
+	}
 
 
 }
